1fe3bc359370e3bd2889390a5ac5634ec03b947e
MKing2k0 <kamai1220000@gmail.com>
2022-02-23 18:46:43 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Main from "./Pages/Main/Main";
+import './Components/Css/zero.css'
+
+export default function App() {
+  return (
+   <>
+     <Main/>
+   </>
+  );
+}
