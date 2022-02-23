e5129df665b99a1db4c726bb5e26e291d1857626
MKing2k0 <kamai1220000@gmail.com>
2022-02-23 20:21:52 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Main from "./Pages/Main/Main";
-import './Components/Css/zero.css'
 
 export default function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import './Components/Css/zero.css'
+import './Components/Css/style.css'
 
 ReactDOM.render(
   <React.StrictMode>
